bf2ad8f2adc59baea98fb8cb04ba705ebe32bbc9
Nathan Krasney <natankrasney@gmail.com>
2020-10-22 11:51:14 +0300

    first version

---
+++ server.js
@@ -0,0 +1,11 @@
+console.log('app is loading ...');
+
+const now = new Date();
+console.log(now.toString());
+console.log(now.toUTCString());
+console.log(now.toTimeString());
+console.log(now.toLocaleTimeString());
+console.log(now.toLocaleString());
+console.log(now.toLocaleDateString());
+console.log(now); 
+
