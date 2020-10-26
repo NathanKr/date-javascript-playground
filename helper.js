51d6adb730cffd1102c9e815e509286b43a4f973
Nathan Krasney <natankrasney@gmail.com>
2020-10-26 22:18:44 +0200

    more options

--- helper.js
+++ helper.js
@@ -4,21 +4,24 @@ function showNow() {
 }
 
 function showDate(preMessage,date) {
-    console.log(`date :                      ${preMessage} ***********`);
+    console.log(`input date :                ${preMessage} ***********`);
     console.log(`date.toString() :           ${date.toString()}`);
+    console.log(`date :                      ${date}`);
+    console.log(`date.toLocaleString() :     ${date.toLocaleString()}`);
     console.log(`date.toUTCString() :        ${date.toUTCString()}`);
+    console.log();
     console.log(`date.toTimeString() :       ${date.toTimeString()}`);
     console.log(`date.toLocaleTimeString() : ${date.toLocaleTimeString()}`);
-    console.log(`date.toLocaleString() :     ${date.toLocaleString()}`);
     console.log(`date.toLocaleDateString() : ${date.toLocaleDateString()}`);
-    console.log(`date :                      ${date}\n`);
+    console.log(`your local timezone offset : ${date.getTimezoneOffset()/60} [hours]`);
+
+    console.log("\n");
   }
 
-function handleTimeZone(){
-    const strDate = 'August 19, 1975 23:15:30 GMT+07:00';
+
+function handleTimeZone(strDate){
     const date1 = new Date(strDate);
     showDate(strDate,date1);
-    console.log(`your local timezone offset : ${date1.getTimezoneOffset()/60} [hours]`);
 }
 
 module.exports = {showNow , handleTimeZone}
--- server.js
+++ server.js
@@ -2,4 +2,8 @@ console.log('app is loading ...\n');
 
 const helper = require('./helper');
 helper.showNow()
-helper.handleTimeZone();
+helper.handleTimeZone('August 19, 1975 23:15:30 GMT+07:00');
+helper.handleTimeZone('Sat Mar 11 2017 11:30:00 GMT+0900');
+helper.handleTimeZone('2017-03-11T11:30:00Z');
+helper.handleTimeZone('2020-10-26 22:12:06 IST');
+helper.handleTimeZone('2020-10-26 22:12:06 GMT+0200');
